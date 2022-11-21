402df332083da8cd3a037f6c76915693ce6c5e5f
Adrià <adrivid98@gmail.com>
2022-11-21 11:04:26 +0100

    creación dado con funcionalidad random al iniciar y al hacer click

--- PR01/src/app/app.module.ts
+++ PR01/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DadoComponent } from './juego/dado/dado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadoComponent
   ],
   imports: [
     BrowserModule,
