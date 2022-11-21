9555106bff0c068b7729f17e1190e7a359adac2e
Adrià <adrivid98@gmail.com>
2022-11-21 13:35:38 +0100

    estructura hecha y aplicando funcionalidad

--- PR01/src/app/calculadora/calc/calc.component.ts
+++ PR01/src/app/calculadora/calc/calc.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calc',
   templateUrl: './calc.component.html',
-  styleUrls: ['./calc.component.css']
+  styleUrls: ['./calc.component.css'],
 })
 export class CalcComponent {
+  primeros: Array<number> = [1, 2, 3];
+  segundos: Array<number> = [4, 5, 6];
+  terceros: Array<number> = [7, 8, 9];
+  n1: number = 0;
+  n2: number = 0;
+  op: string = '';
 
+  constructor() {}
+  getOperador(op:string):void {
+    if (this.op === null) {
+      this.op = op;
+      return;
+  }
+
+  calcular(n1: number, n2: number, op: string): number {
+    let resultado: number;
+    switch (this.op) {
+      case '+':
+        resultado = n1 - n2;
+        break;
+      case '-':
+        resultado = n1 - n2;
+        break;
+      case 'x':
+        resultado = n1 * n2;
+        break;
+      case '/':
+        resultado = n1 / n2;
+        break;
+      default:
+        resultado = 0;
+        break;
+    }
+    return resultado;
+  }
 }
